7fcf71a987debd39befdf85ab62fc06f1c59f2f6
hy.ccccr@gmail.com <>
2024-01-25 21:23:30 +0800

    Add private construction

--- Assets/Scripts/PsychoUnity/Manager/Singleton.cs
+++ Assets/Scripts/PsychoUnity/Manager/Singleton.cs
@@ -5,7 +5,10 @@ namespace PsychoUnity.Manager
     public class Singleton<T> where T : new()
     {
         private static readonly Lazy<T> Core = new Lazy<T>(() => new T());
-
+        
+        // Making Singleton impossible to new
+        private protected Singleton(){}
+        
         public static T Instance => Core.Value;
     }
 }
