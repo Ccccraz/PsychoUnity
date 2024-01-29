0278de5cc326e1443328d5d2e9d1310aae88bf54
Ccccraz <hy.ccccr@gmai.com>
2024-01-29 13:58:25 +0800

    sample: Add the infinite Loop timer example

--- Assets/Plugins/PsychoUnity/Samples/Timer/Timer.cs
+++ Assets/Plugins/PsychoUnity/Samples/Timer/Timer.cs
@@ -5,12 +5,16 @@ namespace PsychoUnity.Samples.Timer
 {
     public class Timer : MonoBehaviour
     {
-        void Start()
+        private void Start()
         {
             TimerManager.Instance.Create("001", TimerManager.TimerType.Normal);
-            TimerManager.Instance.SetSchedule("001", 5000, 0, 5, Task1);
+            TimerManager.Instance.Create("002", TimerManager.TimerType.Normal);
+            TimerManager.Instance.SetSchedule("001", 5000, 10000, 5, Task1);
+            // Infinite loop when times == -1
+            TimerManager.Instance.SetSchedule("002", 1000, 0, -1, Task3);
             TimerManager.Instance.AddTask("001", Task2);
             TimerManager.Instance.Start("001");
+            TimerManager.Instance.Start("002");
         }
 
         private void Update()
@@ -23,22 +27,33 @@ namespace PsychoUnity.Samples.Timer
             if (Input.GetKeyDown(KeyCode.R))
             {
                 TimerManager.Instance.Continue("001");
+                TimerManager.Instance.Continue("002");
             }
 
             if (Input.GetKeyDown(KeyCode.O))
             {
                 TimerManager.Instance.Destroy("001");
             }
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                TimerManager.Instance.Pause("002");
+            }
         }
 
         private void Task1()
         {
-            print("OK");
+            print("Task One");
         }
 
         private void Task2()
         {
-            print("OK again");
+            print("Task Two");
+        }
+
+        private void Task3()
+        {
+            print("looping");
         }
     }
 }
