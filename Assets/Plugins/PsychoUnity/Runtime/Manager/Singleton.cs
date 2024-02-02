9f85001d1e2c8ad8d757bd3c0f0be0cbfab68b19
Ccccraz <hy.ccccr@gmail.com>
2024-02-02 14:24:28 +0800

    doc: add documentation for singleton base class

--- Assets/Plugins/PsychoUnity/Runtime/Manager/Singleton.cs
+++ Assets/Plugins/PsychoUnity/Runtime/Manager/Singleton.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace PsychoUnity.Manager
 {
+    /// <summary>
+    /// Base class for all singleton managers
+    /// </summary>
+    /// <typeparam name="T"> Pass in the class itself that needs to inherit the base class of the singleton. </typeparam>
     public class Singleton<T> where T : new()
     {
         private static readonly Lazy<T> Core = new Lazy<T>(() => new T());
@@ -9,6 +13,9 @@ namespace PsychoUnity.Manager
         // Making Singleton impossible to new
         private protected Singleton(){}
         
+        /// <summary>
+        /// Returns the singleton instance
+        /// </summary>
         public static T Instance => Core.Value;
     }
 }
