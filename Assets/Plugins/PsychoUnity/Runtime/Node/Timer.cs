f9120e94ba1be385bee31e85cfc7a99d15c42e35
Ccccraz <hy.ccccr@gmail.com>
2024-02-24 16:19:14 +0800

    feat: :sparkles: Now the Timer is ready for parameter passing

--- Assets/Plugins/PsychoUnity/Runtime/Node/Timer.cs
+++ Assets/Plugins/PsychoUnity/Runtime/Node/Timer.cs
@@ -1,53 +1,110 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
 namespace PsychoUnity.Node
 {
+    /// <summary>
+    /// A Timer based on asynchronous APIs can launch multiple tasks with a delayed start using a broadcast model.
+    /// </summary>
     [TypeIcon(typeof(Unity.VisualScripting.Timer))]
     [UnitCategory("PsychoUnity")]
     public class Timer : Unit
     {
-        [DoNotSerialize] public ControlInput InputTrigger;
+        /// <summary>
+        /// Number of parameters
+        /// </summary>
+        [SerializeAs(nameof(ArgumentCount))]
+        private int _argumentCount;
 
-        [DoNotSerialize] public ControlOutput OutputTrigger;
+        /// <summary>
+        /// Argument ports
+        /// </summary>
+        [DoNotSerialize] private List<ValueInput> Arguments { get; set; }
+
+        /// <summary>
+        /// Number of parameters
+        /// </summary>
+        [DoNotSerialize]
+        [Inspectable, UnitHeaderInspectable("Arguments")]
+        public int ArgumentCount
+        {
+            get => _argumentCount;
+            set => _argumentCount = Mathf.Clamp(value, 0, 10);
+        }
         
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlInput Enter { get; private set; }
+
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlOutput Exit { get; private set; }
+        
+        /// <summary>
+        /// The scope of influence of the Event
+        /// </summary>
         [DoNotSerialize]
         [PortLabelHidden]
         [NullMeansSelf]
         public ValueInput Target { get; private set; }
 
-        [DoNotSerialize] public ValueInput EventName;
+        /// <summary>
+        /// Event name
+        /// </summary>
+        [DoNotSerialize] public ValueInput Event { get; private set; }
         
-        [DoNotSerialize] public ValueInput Duration;
+        /// <summary>
+        /// The time interval of the Timer
+        /// </summary>
+        [DoNotSerialize] public ValueInput Duration { get; private set; }
         
-        [DoNotSerialize] public ValueInput Delay;
+        /// <summary>
+        /// The amount of time to delay before the first timing starts, with a default of 0
+        /// </summary>
+        [DoNotSerialize] public ValueInput Delay { get; private set; }
         
-        [DoNotSerialize] public ValueInput Times;
-        
+        /// <summary>
+        /// The number of times the timing needs to be repeated, The default is 1
+        /// </summary>
+        [DoNotSerialize] public ValueInput Times { get; private set; }
         
         protected override void Definition()
         {
             Target = ValueInput<GameObject>(nameof(Target), null).NullMeansSelf();
             
-            EventName = ValueInput<string>("Event", string.Empty);
-            Duration = ValueInput<int>("Duration", 0);
-            Delay = ValueInput<int>("Delay", 0);
-            Times = ValueInput<int>("Times", 1);
+            Event = ValueInput(nameof(Event), string.Empty);
+            Duration = ValueInput(nameof(Duration), 0);
+            Delay = ValueInput(nameof(Delay), 0);
+            Times = ValueInput(nameof(Times), 1);
 
-            InputTrigger = ControlInput(" ", flow =>
+            Arguments = new List<ValueInput>();
+
+            for (var i = 0; i < ArgumentCount; i++)
             {
-                var eventName = flow.GetValue<string>(EventName);
-                var duration = flow.GetValue<int>(Duration);
-                var delay = flow.GetValue<int>(Delay);
-                var times = flow.GetValue<int>(Times);
-                
-                PsychoUnity.Timer.Timing(duration, delay, times, () => EventBus.Trigger(eventName, 0));
-                
-                return OutputTrigger;
-            });
+                var argument = ValueInput<object>($"Arg{i}");
+                Arguments.Add(argument);
+            }
+
+            Enter = ControlInput(nameof(Enter), Trigger);
             
-            OutputTrigger = ControlOutput(" ");
+            Exit = ControlOutput(nameof(Exit));
+        }
+
+        private ControlOutput Trigger(Flow flow)
+        {
+            var eventName = flow.GetValue<string>(Event);
+            var duration = flow.GetValue<int>(Duration);
+            var delay = flow.GetValue<int>(Delay);
+            var times = flow.GetValue<int>(Times);
+            var target = flow.GetValue<GameObject>(Target);
+            var arguments = Arguments.Select(flow.GetConvertedValue).ToArray();
+            
+            PsychoUnity.Timer.Timing(duration, delay, times,
+                () => CustomEvent.Trigger(target, eventName, arguments));
+            
+            return Exit;
         }
     }
 }
