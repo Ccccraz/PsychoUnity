683b5f5d8db034bb7d8c216a3e106275fbcb1f8c
ccccr <hy.ccccr@gmail.com>
2024-08-08 23:54:26 +0800

    feat: :sparkles: added a mini web server

---
+++ Runtime/MiniHttpServer.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using PsychoUnity.Manager;
+using UnityEngine;
+
+namespace PsychoUnity
+{
+    public class MiniHttpServer : Singleton<MiniHttpServer>
+    {
+        private HttpListener _listener;
+        private bool _isRunning;
+        
+        public void Create(string[] prefixes)
+        {
+            _listener = new HttpListener();
+            
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+            
+            foreach (var variable in prefixes)
+            {
+                _listener.Prefixes.Add(variable);
+            }
+        }
+
+        public async Task StartAsync()
+        {
+            _listener.Start();
+            _isRunning = true;
+            
+            while (_isRunning)
+            {
+                try
+                {
+                    var context = await _listener.GetContextAsync();
+                    ProcessRequest(context);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogWarning(e);
+                    throw;
+                }
+                finally
+                {
+                    _isRunning = false;
+                    _listener.Abort();
+                }
+            }
+        }
+        
+        private static void ProcessRequest(HttpListenerContext context)
+        {
+            var request = context.Request;
+            var url = request.Url.AbsolutePath;
+            
+            EventManager.Instance.EventTrigger(url, request);
+        }
+    }
+}
