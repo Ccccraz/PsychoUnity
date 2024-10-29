09bf8e91b6b6b113c9ef591ad8360899fc93286d
Ccccraz <hy.ccccr@gmail.com>
2024-10-29 22:05:17 +0800

    feat: :sparkles: new recorder for json file

--- Runtime/Recorder.cs
+++ Runtime/Recorder.cs
@@ -8,22 +8,22 @@ namespace PsychoUnity
     {
         public static void CreateRecorder(string name, IRecorderData data, [CanBeNull] string custom, string prefix = "Assets/Data")
         {
-            RecordManager.Instance.Create(name, data, custom, prefix);
+            RecorderManager.Instance.Create(name, data, custom, prefix);
         }
 
         internal static void CreateRecorderNode(string name, IDictionary data, [CanBeNull] string custom = null, string prefix = "Assets/Data")
         {
-            RecordManager.Instance.CreateNode(name, data, custom, prefix);
+            RecorderManager.Instance.CreateNode(name, data, custom, prefix);
         }
         
         public static void Write(string name)
         {
-            RecordManager.Instance.Write(name);
+            RecorderManager.Instance.Write(name);
         }
 
         public static void Destroy(string name)
         {
-            RecordManager.Instance.Destroy(name);
+            RecorderManager.Instance.Destroy(name);
         }
     }
 }
